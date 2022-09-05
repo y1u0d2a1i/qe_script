89866d5d20f413a0db63cc9f0ef3e1c6857a9208
y1u0d2a1i <y1u0d2a1i@icloud.com>
2022-09-05 21:32:18 +0900

    add: load qel from binary file

--- scf/qelattice.py
+++ scf/qelattice.py
@@ -1,10 +1,20 @@
 import os
+import pickle
 
 from scf.get_lattice_info import QELattice
 from scf.get_relax_lattice_info import RelaxQELattice
 
 
+def get_qel_from_bin(path2target, bin_filename='qel.bin'):
+    with open(os.path.join(path2target, bin_filename), 'rb') as p:
+        qel = pickle.load(p)
+    return qel
+
+
 def get_qel(path2target):
+    if os.path.exists(os.path.join(path2target, 'qel.bin')):
+        return get_qel_from_bin(path2target)
+
     if os.path.exists(os.path.join(path2target, 'scf.in')):
         return QELattice(path2target) 
     elif os.path.exists(os.path.join(path2target, 'relax.in')):
