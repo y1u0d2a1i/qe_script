7f1b7fc4371dc6991ba20bf1c998a0a095482f1b
y1u0d2a1i <y1u0d2a1i@icloud.com>
2022-09-05 18:32:39 +0900

    modified: add exception

--- scf/qelattice.py
+++ scf/qelattice.py
@@ -5,7 +5,9 @@ from scf.get_relax_lattice_info import RelaxQELattice
 
 
 def get_qel(path2target):
-    if os.path.exists(os.path.join(path2target, 'relax.in')):
+    if os.path.exists(os.path.join(path2target, 'scf.in')):
+        return QELattice(path2target) 
+    elif os.path.exists(os.path.join(path2target, 'relax.in')):
         return RelaxQELattice(path2target)
     else:
-        return QELattice(path2target)
+        raise Exception('files in target directory is not enough')
