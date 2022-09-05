98d21abb95bae0af9541fcb7a8bd8a81749b2703
y1u0d2a1i <y1u0d2a1i@icloud.com>
2022-09-05 17:58:12 +0900

    QELatticeの切り替え実装

---
+++ scf/qelattice.py
@@ -0,0 +1,11 @@
+import os
+
+from scf.get_lattice_info import QELattice
+from scf.get_relax_lattice_info import RelaxQELattice
+
+
+def get_qel(path2target):
+    if os.path.exists(os.path.join(path2target, 'relax.in')):
+        return RelaxQELattice(path2target)
+    else:
+        return QELattice(path2target)
